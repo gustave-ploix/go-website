d5944ccbf12f851fee26a6d88da284979a099574
gustave-ploix <gustaveploix.pro@gmail.com>
2022-01-06 16:34:43 +0100

    created a container component witch contains all the website

---
+++ src/Container/index.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+import Preloader from "../components/Preloader";
+import FullPage from "../components/FullPage";
+
+import "./container.scss";
+
+export default function BlackScreen() {
+  const [isClicked, setIsClicked] = useState(false);
+  const [animationIsDone, setAnimationIsDone] = useState(false);
+  const [transitionIsDone, setTransitionIsDone] = useState(false);
+
+  return (
+    <div className="black">
+      {transitionIsDone ? (
+        <FullPage isClicked={isClicked} transitionIsDone={transitionIsDone} />
+      ) : (
+        <Preloader
+          isClicked={isClicked}
+          setIsClicked={setIsClicked}
+          animationIsDone={animationIsDone}
+          setAnimationIsDone={setAnimationIsDone}
+          setTransitionIsDone={setTransitionIsDone}
+        />
+      )}
+    </div>
+  );
+}
