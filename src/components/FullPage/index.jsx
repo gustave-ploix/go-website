1fbcc62c3ebf256f79223f877cc15925cc45bbe4
gustave-ploix <gustaveploix.pro@gmail.com>
2022-01-25 19:29:46 +0100

    added some elements to the full page component.

--- src/components/FullPage/index.jsx
+++ src/components/FullPage/index.jsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 import Navbar from '../Navbar';
+import FirstElement from '../FirstElement';
 
 
 export default function FullPage({ 
@@ -6,13 +9,23 @@ export default function FullPage({
     transitionIsDone
 }) {
 
+    const [mouseX, setMouseX] = useState(0);
+
+
+    const handleMove = (e) => {
+        setMouseX(e.screenX);
+        console.log(mouseX);
+    }
 
     return (
-        <>
+        <div
+        onMouseMove={handleMove}
+        >
             <Navbar
                 isClicked={isClicked}
                 transitionIsDone={transitionIsDone}
             />
-        </>
+            <FirstElement transitionIsDone={transitionIsDone}/>
+        </div>
     )
 }
