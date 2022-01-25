57040bad14d31384e6cc27d3d45f5853c8e61779
gustave-ploix <gustaveploix.pro@gmail.com>
2022-01-25 14:42:51 +0100

    created and styled the navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,20 +1,9 @@
-import { useEffect } from "react";
-
 import "./title.scss";
 
 export default function Title({
   isClicked,
-  setTransitionIsDone,
   transitionIsDone,
 }) {
-  useEffect(() => {
-    if (isClicked == true) {
-      setTimeout(() => {
-        setTransitionIsDone(!transitionIsDone);
-      }, 2500);
-    }
-  }, [isClicked]);
-
   return (
     <div className={isClicked ? "titleBox visible" : "titleBox"}>
       <div className={transitionIsDone ? "line deployed" : "line"}></div>
